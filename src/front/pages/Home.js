bdf1b8a296823ed774b6b6ada254f3137a5909a0
Jennifer Proust <jennifer.proust@gmail.com>
2019-10-07 17:35:56 +0200

    feat(weather): remove the page and the routee /weather

--- src/front/pages/Home.js
+++ src/front/pages/Home.js
@@ -15,8 +15,7 @@ export default class Home extends React.Component {
             <div className="section no-pad-bot">
                 <div className="container">
                     <h1>{ this.props.title }</h1>
-                    <a className="waves-effect waves-light btn-large" href="/weather" >Wanna know the weather in Paris?</a>
-                    <a className="waves-effect waves-light btn-large" href="/darksky" >Weather in SSR ?</a>
+                    <a className="waves-effect waves-light btn-large" href="/darksky" >Wanna know the weather in Paris?</a>
                     <a className="waves-effect waves-light btn-large" href="/hue" >Wanna control the lights?</a>
                     <a className="waves-effect waves-light btn-large" onClick={this.onClick} >ACTION!</a>
                 </div>
--- src/routes/_.js
+++ src/routes/_.js
@@ -1,11 +1,9 @@
 import index from './index';
-import weather from './weather';
 import hue from './hue';
 import darksky from './darksky';
 
 export default {
     index,
-    weather,
     hue,
     darksky
 };
