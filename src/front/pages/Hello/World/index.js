7a3711985726b31264d084459eb1e06994d83699
Jennifer Proust <jennifer.proust@gmail.com>
2019-11-10 13:43:04 +0100

    refactor(hello): remove hello route and hello components

--- src/front/pages/Hello/World/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const World = () => (
-  <div>Hello World</div>
-);
-
-export default World;
--- src/front/pages/Hello/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Hello = () => (
-  <div>Hello Hello</div>
-);
-
-export default Hello;
